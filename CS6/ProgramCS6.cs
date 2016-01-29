17f8b9f98f6dbd1a1d4687b68b5a09c7d55e4bed
Berndt Hamboeck <hamboeck@soft-dat.com>
2016-01-29 13:57:54 +0100

    initial checkin

---
+++ CS6/ProgramCS6.cs
@@ -0,0 +1,285 @@
+﻿//using System;
+//using static System.Console;
+
+
+//namespace CS6
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            WriteLine("Hello world!");
+//            ReadLine();
+//        }
+
+//    }
+//}
+
+
+//Example 1
+//using static System.Console;
+//using static CS6.SomeOtherClass;
+
+//namespace CS6
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            WriteLine("Hello world!");
+//            HelloClass();
+//            ReadLine();
+//        }
+//    }
+
+//    static class SomeOtherClass
+//    {
+//        public static void HelloClass()
+//        {
+//            WriteLine("Hello universe!");
+//        }
+//    }
+//}
+
+
+//Example 2
+//using System;
+//using static System.Console;
+
+//namespace CS6
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            Employee employee = new Employee();
+//            WriteLine(employee.ID);
+//            ReadLine();
+//        }
+//    }
+
+//    public class Employee
+//    {
+//        public Guid ID { get; set; } = Guid.NewGuid();
+//    }
+
+//}
+
+//Example 3
+//using System.Collections.Generic;
+//using static System.Console;
+
+//namespace CS6
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            var player = new Dictionary<string, string>()
+//            {
+//                ["Lionel Messi"] = "Argentinien",
+//                ["Cristiano Ronaldo"] = "Portugal",
+//                ["David Alaba"] = "Österreich"
+//            };
+
+//            foreach (KeyValuePair<string, string> keyValuePair in player)
+//            {
+//                WriteLine(keyValuePair.Key + ": " +
+//                keyValuePair.Value + "\n");
+//            }
+
+//            ReadLine();
+//        }
+//    }
+//}
+
+//Example 4
+using static System.Console;
+
+//namespace CS6
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            string firstName = "Berndt";
+//            string lastName = "Hamböck";
+
+//            WriteLine($"Ich heisse {firstName} {lastName}!");
+
+//            ReadLine();
+//        }
+//    }
+//}
+
+//Example 5
+
+//using System;
+//using static System.Console;
+
+//namespace CS6
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            string name = null;
+//            DoSomething(name);
+//            ReadLine();
+//        }
+
+//        public static void DoSomething(string nameParam)
+//        {
+//            if (nameParam == null)
+//                throw new ArgumentException(nameof(nameParam) + " is null");
+//        }
+//    }
+//}
+
+
+//using static System.Console;
+
+//namespace CSBefore6
+//{
+//    class Program
+//    {
+//        private static double MultiplyNumbers(double num1, double num2)
+//            => num1 * num2;
+
+//        static void Main(string[] args)
+//        {
+//            double num1 = 5;
+//            double num2 = 10;
+
+//            WriteLine(MultiplyNumbers(num1, num2));
+//            ReadLine();
+//        }
+//    }
+//}
+
+
+//Example 6
+//using System;
+
+//namespace CS6
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            var httpStatusCode = 404;
+//            Write("Exception type: ");
+
+//            try
+//            {
+//                throw new Exception(httpStatusCode.ToString());
+//            }
+//            catch (Exception ex) when (ex.Message.Equals("400"))
+//            {
+//                Write("Bad Request");
+//                ReadLine();
+//            }
+//            catch (Exception ex) when (ex.Message.Equals("401"))
+//            {
+//                Write("Unauthorized");
+//                ReadLine();
+//            }
+//            catch (Exception ex) when (ex.Message.Equals("402"))
+//            {
+//                Write("Payment Required");
+//                ReadLine();
+//            }
+//            catch (Exception ex) when (ex.Message.Equals("403"))
+//            {
+//                Write("Forbidden");
+//                ReadLine();
+//            }
+//            catch (Exception ex) when (ex.Message.Equals("404"))
+//            {
+//                Write("Not Found");
+//                ReadLine();
+//            }
+
+//            ReadLine();
+//        }
+//    }
+//}
+
+//Example 7
+using System;
+using System.Net;
+
+namespace CS6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CallWebSite();
+        }
+
+        private static async void CallWebSite()
+        {
+            WebClient wc = new WebClient();
+            string result;
+            try
+            {
+                result = await wc.DownloadStringTaskAsync(new Uri("http://badurl"));
+            }
+            catch
+            {
+                //pre C# 6 -> error CS1985: Cannot await in the body of a catch clause
+                result = await wc.DownloadStringTaskAsync(new Uri("http://fallbackurl"));
+            }
+        }
+    }
+}
+
+//Example 8
+//using static System.Console;
+
+
+//namespace CS6
+//{
+//    class Program
+//    {
+//        private static double MultiplyNumbers(double num1, double num2) => num1 * num2;
+//        static void Main(string[] args)
+//        {
+//            double num1 = 5;
+//            double num2 = 10;
+
+//            WriteLine(MultiplyNumbers(num1, num2));
+//            ReadLine();
+//        }
+//    }
+//}
+
+//Example 9
+//using System;
+//using static System.Console;
+
+
+//namespace CS6
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            Person person = new Person();
+//            if (person.Name == String.Empty)
+//            {
+//                person = null;
+//            }
+
+//            WriteLine(person?.Name ?? "Field is null.");
+
+//            ReadLine();
+//        }
+//    }
+
+//    public class Person
+//    {
+//        public string Name { get; set; } = "";
+//    }
+//}
---
+++ CSBefore/ProgramBeforeCS6.cs
@@ -0,0 +1,205 @@
+﻿using System;
+
+namespace CSBefore6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world!");
+        }
+    }
+}
+
+
+//Example 2
+//using System;
+
+//namespace CSBefore6
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            Employee employee = new Employee();
+//            Console.WriteLine(employee.ID);
+//            Console.ReadLine();
+//        }
+//    }
+
+//    public class Employee
+//    {
+//        public Employee()
+//        {
+//            ID = Guid.NewGuid();
+//        }
+
+//        public Guid ID { get; set; }
+//    }
+
+//}
+
+
+
+//Example 3
+//using System;
+//using System.Collections.Generic;
+
+//namespace CSBefore6
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            var stars = new Dictionary<string, string>()
+//            {
+//                 { "Lionel Messi", "Argentinien" },
+//                 { "Cristiano Ronaldo", "Portugal" },
+//                 { "David Alaba", "Österreich" }
+//            };
+
+//            foreach (KeyValuePair<string, string> keyValuePair in stars)
+//            {
+//                Console.WriteLine(keyValuePair.Key + ": " +
+//                keyValuePair.Value + "\n");
+//            }
+
+//            Console.ReadLine();
+//        }
+//    }
+//}
+
+//Example 4
+//using System;
+
+//namespace CSBefore6
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            string firstName = "Berndt";
+//            string lastName = "Hamböck";
+
+//            Console.WriteLine("Name : " + firstName + " " + lastName);
+//            Console.WriteLine("Name : {0} {1}", firstName, lastName);
+
+//            Console.ReadLine();
+//        }
+//    }
+//}
+
+//Example 5
+//using System;
+
+//namespace CSBefore6
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            string name = null;
+//            DoSomething(name);
+//            Console.ReadLine();
+//        }
+
+//        public static void DoSomething(string nameParam)
+//        {
+//            if (nameParam == null)
+//                throw new ArgumentException("name is null");
+//        }
+//    }
+//}
+
+//Example 6
+//using static System.Console;
+
+//namespace CSBefore6
+//{
+//    class Program
+//    {
+//        private static double MultiplyNumbers(double num1, double num2)
+//        {
+//            return num1 * num2;
+//        }
+
+//        static void Main(string[] args)
+//        {
+//            double num1 = 5;
+//            double num2 = 10;
+
+//            WriteLine(MultiplyNumbers(num1, num2));
+//            ReadLine();
+//        }
+//    }
+//}
+
+//Example 7
+//using System;
+//using static System.Console;
+
+
+//namespace CSBefore6
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            var httpStatusCode = 404;
+//            Write("HTTP Error: ");
+
+//            try
+//            {
+//                throw new Exception(httpStatusCode.ToString());
+//            }
+//            catch (Exception ex)
+//            {
+//                if (ex.Message.Equals("500"))
+//                    Write("Bad Request");
+//                else if (ex.Message.Equals("401"))
+//                    Write("Unauthorized");
+//                else if (ex.Message.Equals("402"))
+//                    Write("Payment Required");
+//                else if (ex.Message.Equals("403"))
+//                    Write("Forbidden");
+//                else if (ex.Message.Equals("404"))
+//                    Write("Not Found");
+//            }
+
+//            ReadLine();
+//        }
+//    }
+//}
+
+//Example 8
+//none
+
+//Example 9
+//using System;
+//using System.Net;
+//using System.Threading.Tasks;
+
+//namespace CSBefore6
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            CallWebSite();
+//        }
+
+//        private static async void CallWebSite()
+//        {
+//            WebClient wc = new WebClient();
+//            Task<string> downloadTask = wc.DownloadStringTaskAsync(new Uri("http://badurl"));
+//            downloadTask = downloadTask.ContinueWith(
+//                t =>
+//                {
+//                    return wc.DownloadStringTaskAsync(new Uri("http://fallbackurl")).Result;
+//                }, TaskContinuationOptions.OnlyOnFaulted);
+
+//            var result = await downloadTask;
+//        }
+//    }
+
+//}
